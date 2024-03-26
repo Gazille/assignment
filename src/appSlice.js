d519ccb6455cc97545fca3dd6ef223c95b958734
Tuan <>
2024-03-26 18:06:48 +0700

    update payload logout

--- src/appSlice.js
+++ src/appSlice.js
@@ -68,7 +68,7 @@ export const logout = createAsyncThunk(
       },
       {
         headers: {
-          Authorization: payload.accessToken,
+          Authorization: `Bearer ${payload.accessToken}`,
         },
       }
     );
